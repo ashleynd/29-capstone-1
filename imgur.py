73d3c9f754102ad3143fbf331392c696d0090f5d
Ashley Dragan <ashleydragan@Ashleys-MacBook-Air.local>
2021-06-09 21:31:55 -0400

    adding in success data gathering from API

--- imgur.py
+++ imgur.py
@@ -1,4 +1,100 @@
-# from imgurpython import ImgurClient
+import requests
+from imgurpython import ImgurClient
+
+# If you already have an access/refresh pair in hand
+client_id = '9b315a3d4a73278'
+client_secret = '6ea5304b64c0ce2d995277b3a125f752a059ca1c'
+access_token = 'bf69af937f3e5a129dfa5d0b665f6eefff6e1af2'
+refresh_token = 'a892e7545a3174914f55ecf4b1e210fc78f27f2c'
+
+# Note since access tokens expire after an hour, only the refresh token is required (library handles autorefresh)
+client = ImgurClient(client_id, client_secret, access_token, refresh_token)
+
+# Extracts the items (images) on the front page of Imgur:
+items = client.gallery(section='top', sort='time', page=3, window='week', show_viral=False)
+for item in items:
+    print(item.link)
+
+    # print(item.title)
+    # print(items)
+    # jinja for loop to iterate through each one to make a card
+
+# Find the image on the front page that has the highest number of views 
+# items = client.gallery()
+# max_item = None
+# max_views = 0
+# for item in items:
+#     if item.views > max_views:
+#         max_item = item
+#         max_views = item.views
+# print(max_item.title)
+# print(max_views)
+
+
+
+
+
+
+# url = "https://api.imgur.com/3/album/300"
+
+# headers = {'Authorization': 'Bearer 3af336150a146a8d15fb0f2717ae78a26a2793fe', 'Content-type': 'application/json'}
+
+# response = requests.request("GET", url, headers=headers)
+
+# print(response)
+
+
+# from os import path
+# from imgur_python import Imgur
+
+# from flask import Flask
+# from models import connect_db
+
+# app = Flask(__name__)
+# connect_db(app)
+
+# API_BASE_URL ="https://api.imgur.com/3"
+
+# imgur_client = Imgur({'client_id': '9b315a3d4a73278'})
+
+
+# @app.route('{API_BASE_URL}/gallery/search', 
+#                                 params={'q': 'cats'})
+# def user_search():
+#     """User search queries."""
+
+
+# @app.route('{API_BASE_URL}/gallery/image', 
+#                                 params={'title': 'cats'})
+# def post_image():
+#     """Post image to gallery."""
+
+
+# from os import path
+# from imgur_python import Imgur
+# import requests
+
+# # API_BASE_URL ="https://api.imgur.com/3"
+
+# imgur_client = Imgur({'client_id': '9b315a3d4a73278'})
+# image = imgur_client.image_upload(path.realpath('https://static8.depositphotos.com/1007254/986/i/950/depositphotos_9866977-stock-photo-flower-pots.jpg'), 'Untitled', 'My first image upload')
+# image_id = image['response']['data']['id']
+# album =  imgur_client.album_create([image_id], 'My first album', 'Something funny', 'public')
+# album_id = album['response']['data']['id']
+# response = imgur_client.gallery_album(album_id, 'This is going down on the sub', 0, 'funny,midly_interesting')
+# print(response)
+
+
+
+
+
+
+
+
+
+
+
+# # from imgurpython import ImgurClient
 
 # # If you already have an access/refresh pair in hand
 # client_id = '9b315a3d4a73278'
@@ -22,27 +118,10 @@
 
 # client = ImgurClient(client_id, client_secret)
 
-# items = client.gallery()
+# items = client.gallery(section='top', sort='time', page=3, window='week', show_viral=False)
 # for item in items:
 #     print(item.link)
-#     print(item.title)
-#     print(item.views)
 
-
-
-# from os import path
-# from imgur_python import Imgur
-# import requests
-
-# API_BASE_URL ="https://api.imgur.com/3"
-
-# imgur_client = Imgur({'client_id': '9b315a3d4a73278'})
-# image = imgur_client.image_upload(path.realpath('./image.png'), 'Untitled', 'My first image upload')
-# image_id = image['response']['data']['id']
-# album =  imgur_client.album_create([image_id], 'My first album', 'Something funny', 'public')
-# album_id = album['response']['data']['id']
-# response = imgur_client.gallery_album(album_id, 'This is going down on the sub', 0, 'funny,midly_interesting')
-# print(response)
 
 
 # @app.route('{API_BASE_URL}/gallery/search', 
