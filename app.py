da07bb7e7e74f562aeb54854b93a648d5eae71a1
Ashley Dragan <ashleydragan@Ashleys-MacBook-Air.local>
2021-06-23 12:45:49 -0400

    add heroku psql url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, session, flash, abort, url_for
+from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Post
 from forms import RegisterForm, LoginForm, AddPostForm, DeleteForm, EditPostForm
@@ -7,14 +7,10 @@ import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///picslink"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "postgresql:///picslink")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY', 'hellosecret1')
-print('****************************')
-print('****************************')
-print('****************************')
-print(app.config["SECRET_KEY"])
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 toolbar = DebugToolbarExtension(app)
